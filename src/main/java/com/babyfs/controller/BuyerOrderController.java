fd76205560d3ef3c7b312faf8b9590cb7ebe2d88
wangchao25 <wangchao25@baidu.com>
2018-03-23 19:53:10 +0800

    订单列表

--- src/main/java/com/babyfs/controller/BuyerOrderController.java
+++ src/main/java/com/babyfs/controller/BuyerOrderController.java
@@ -7,14 +7,21 @@ import com.babyfs.enums.ResultEnum;
 import com.babyfs.exception.SellException;
 import com.babyfs.form.OrderForm;
 import com.babyfs.service.OrderService;
+import com.babyfs.utils.ResultVOUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/buyer/order")
@@ -32,11 +39,36 @@ public class BuyerOrderController {
                     bindingResult.getFieldError().getDefaultMessage());
         }
         OrderDTO orderDTO = OrderFormToOrderDTOConverter.convert(orderForm);
-        
-
-        return new ResultVO();
+        if (CollectionUtils.isEmpty(orderDTO.getOrderDetailList())){
+            log.error("【创建订单】购物车不能为空");
+            throw new SellException(ResultEnum.CART_EMPTY);
+        }
+        OrderDTO createResult = orderService.create(orderDTO);
+        Map<String, String> map = new HashMap<>();
+        map.put("orderId",createResult.getOrderId());
+        return ResultVOUtil.success(map);
     }
-    // 订单列表
+
+    /**
+     *
+     * @param openid
+     * @param page
+     * @param size
+     * @return List<OrderDTO>
+     */
+    @GetMapping("list")
+    public ResultVO list(@RequestParam("openid") String openid,
+                                         @RequestParam(value = "page",defaultValue = "0") Integer page,
+                                         @RequestParam(value = "size", defaultValue = "10") Integer size){
+        if (StringUtils.isEmpty(openid)) {
+            log.error("【查询订单列表】openid为空");
+            throw new SellException(ResultEnum.PARAM_ERROR);
+        }
+        PageRequest request = PageRequest.of(page, size);
+        Page<OrderDTO> orderDTOList = orderService.findList(openid, request);
+        return ResultVOUtil.success(orderDTOList.getContent());
+    }
     // 订单详情
+
     // 取消订单
 }
